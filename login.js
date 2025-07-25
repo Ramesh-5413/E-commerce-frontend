2f5245a9c16a1708dcb50950d59e1150ecd4931a
= <=>
2025-07-25 12:01:13 +0530

    registe and login

---
+++ login.js
@@ -0,0 +1,6 @@
+const login = ()=>{
+    return `
+    <h1>iam login</h1>
+    `
+}
+export default login
---
+++ register.js
@@ -0,0 +1,6 @@
+const register = ()=>{
+    return `
+    <h1>iam register</h1>
+    `
+}
+export default register
